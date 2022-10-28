5c08f4dbdf8f8d668ee73993bac27b1e551599f5
Krishna Mahat <krishnamahat@Krishnas-MacBook-Air.local>
2022-10-28 22:20:12 +1030

    first commit

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const app =express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require( "./routes/auth");
+const userRoute = require( "./routes/users");
  
 dotenv.config();
 app.use(express.json())
@@ -14,9 +15,9 @@ mongoose.connect(process.env.MONGO_URL,{
 .then(console.log("connnected to MONGO DB"))
 .catch((err)=>console.log(err));
 
-
-app.use("/api/auth", authRoute);
-
 app.listen("3000",()=>{
     console.log("Back end is running");
 });
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
