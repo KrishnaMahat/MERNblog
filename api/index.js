ce7c7c3eb63eac2318311305eda0c65cd0642331
Krishna Mahat <krishnamahat@Krishnas-MacBook-Air.local>
2023-02-14 13:36:40 +1030

    First commit

--- api/index.js
+++ api/index.js
@@ -4,16 +4,41 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require( "./routes/auth");
 const userRoute = require( "./routes/users");
+const postRoute = require( "./routes/posts");
+const catRoute = require( "./routes/categories");
+const multer =require("multer");
+const path = require("path");
+
+
+
  
 dotenv.config();
 app.use(express.json())
+app.use("/images", express.static(path.join(__dirname, "/images")));
 
 mongoose.connect(process.env.MONGO_URL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useCreateIndex: true,
+    // useFindAndModify:true
 })
 .then(console.log("connnected to MONGO DB"))
 .catch((err)=>console.log(err));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, req.body.name);
+    },
+  });
+
+  const upload = multer({ storage: storage });
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+  });
+
 
 app.listen("3000",()=>{
     console.log("Back end is running");
@@ -21,3 +46,5 @@ app.listen("3000",()=>{
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", catRoute);
